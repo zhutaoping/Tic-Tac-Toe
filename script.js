f4560a7ba9e512b2cbe247909639d5197cdb2e31
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-03-22 01:13:35 +0800

    logic parts done

--- script.js
+++ script.js
@@ -5,40 +5,52 @@ const grids = document.querySelector(".grid");
 const imgCross = "./images/cross.png";
 const imgCircle = "./images/circle.png";
 
+const player = (sign) => {
+  const name = sign;
+  const moves = [];
+  return { moves, name };
+};
+
 const game = () => {
-  const gameMoves = [];
-  const gameMoves2 = [{ circle: 6 }, { circle: 7 }, { circle: 2 }];
   let curSign = imgCircle;
+  let curPlayer = circlePlayer;
 
   board.addEventListener("click", _renderSign);
 
   function _renderSign(e) {
     const gridEl = e.target.closest(".grid");
     gridEl.innerHTML = `<img src='${curSign}'>`;
+    curPlayer.moves.push(+e.target.id);
 
-    _storeMove2Arr(+e.target.id);
+    _checkStatus();
+    _nextPlayer();
   }
 
-  function _storeMove2Arr(id) {
-    const curMove = {
-      [curSign === imgCircle ? "circle" : "cross"]: id,
-    };
-    gameMoves.push(curMove);
-
-    _nextTurnSign();
-  }
-
-  function _nextTurnSign() {
+  function _nextPlayer() {
     curSign = curSign === imgCircle ? imgCross : imgCircle;
-
-    if (gameMoves.length > 4) _checkGameStatus();
+    curPlayer = curPlayer === circlePlayer ? crossPlayer : circlePlayer;
   }
 
-  function _checkGameStatus() {
-    console.log("checking");
+  function _checkStatus() {
+    const winIdNums = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+    // console.log(curPlayer);
+    winIdNums.forEach((el) => {
+      if (el.every((e) => curPlayer.moves.includes(e)))
+        console.log(`${curPlayer.name.toUpperCase()} WIN!`);
+    });
   }
-
-  return { gameMoves2 };
 };
+
+const circlePlayer = player("circle");
+const crossPlayer = player("cross");
 
 game();
