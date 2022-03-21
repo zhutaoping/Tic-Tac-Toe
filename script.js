09b1baf52f03356636677aded19438198bbce139
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-03-21 23:07:28 +0800

    rename png

--- script.js
+++ script.js
@@ -2,12 +2,43 @@
 
 const board = document.querySelector(".board");
 const grids = document.querySelector(".grid");
+const imgCross = "./images/cross.png";
+const imgCircle = "./images/circle.png";
 
-const gameBoard = (() => {
+const game = () => {
   const gameMoves = [];
-})();
+  const gameMoves2 = [{ circle: 6 }, { circle: 7 }, { circle: 2 }];
+  let curSign = imgCircle;
 
-board.addEventListener("click", function (e) {
-  const gridEl = e.target.closest(".grid");
-  gridEl.innerHTML = `<img src='./images/circle.png'>`;
-});
+  board.addEventListener("click", _renderSign);
+
+  function _renderSign(e) {
+    const gridEl = e.target.closest(".grid");
+    gridEl.innerHTML = `<img src='${curSign}'>`;
+
+    _storeMove2Arr(+e.target.id);
+  }
+
+  function _storeMove2Arr(id) {
+    const curMove = {
+      [curSign === imgCircle ? "circle" : "cross"]: id,
+    };
+    gameMoves.push(curMove);
+
+    _nextTurnSign();
+  }
+
+  function _nextTurnSign() {
+    curSign = curSign === imgCircle ? imgCross : imgCircle;
+
+    if (gameMoves.length > 4) _checkGameStatus();
+  }
+
+  function _checkGameStatus() {
+    console.log("checking");
+  }
+
+  return { gameMoves2 };
+};
+
+game();
