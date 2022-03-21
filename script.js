80c65431394cfda5af4325eecb59e01b7fd087a0
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2022-03-22 03:46:30 +0800

    reset via reload()

--- script.js
+++ script.js
@@ -9,14 +9,17 @@ const player = (sign) => {
 const game = () => {
   const board = document.querySelector(".board");
   const grids = document.querySelectorAll(".grid");
+  const resetBtn = document.querySelector("button");
 
   const imgCross = "./images/cross.png";
   const imgCircle = "./images/circle.png";
 
   let curSign = imgCircle;
   let curPlayer = circlePlayer;
+  let gameOver = false;
 
   board.addEventListener("click", _renderSign);
+  resetBtn.addEventListener("click", _init);
 
   function _renderSign(e) {
     const gridEl = e.target.closest(".grid");
@@ -43,7 +46,7 @@ const game = () => {
     numsOnLine.forEach((el) => {
       if (el.every((e) => curPlayer.moves.includes(e))) {
         _winnerScreen();
-        // console.log(`${curPlayer.name.toUpperCase()} WIN!`);
+        // console.log(`${curPlayer.name} WIN!`);
       }
     });
   }
@@ -55,15 +58,29 @@ const game = () => {
   }
 
   function _winnerScreen() {
-    grids.forEach((el) => (el.style.display = "none"));
+    // grids.forEach((el) => (el.style.display = "none"));
     board.style.background = "teal";
     board.style.display = "flex";
     board.style.alignItems = "center";
     board.innerHTML = `<img src='${curSign}' width= '20%' height="20%">`;
     board.innerHTML += "<h1>WIN!</h1>";
+    gameOver = true;
+    board.removeEventListener("click", _renderSign);
+  }
+
+  function _init() {
+    circlePlayer.moves = [];
+    crossPlayer.moves = [];
+
+    if (gameOver === false) {
+      grids.forEach((grid) => (grid.innerHTML = ""));
+      return;
+    }
+
+    location.reload();
   }
 };
 
-const circlePlayer = player("circle");
-const crossPlayer = player("cross");
+const circlePlayer = player("Circle");
+const crossPlayer = player("Cross");
 game();
